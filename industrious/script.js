8eb7a3995bad217773d9a4ba45b860931395b3d2
shikhu51197 <sg780060@gmail.com>
2022-07-23 11:29:49 +0530

    done

---
+++ industrious/script.js
@@ -0,0 +1,16 @@
+let productContainers = [...document.querySelectorAll('.product-container')];
+let  nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+let  preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
